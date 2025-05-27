1659385d5a5d35ace3398ed85b9f39d1c40031c3
soongu <hsg9984@gmail.com>
2025-05-27 11:58:25 +0900

    변수와 데이터타입 - 문자열, 논리, 기타 타입, 형 변환

---
+++ 1-3-data-type/3-other-types.js
@@ -0,0 +1,31 @@
+
+// 논리 타입
+let flag = false;
+console.log(typeof flag);
+
+let result = 10 > 5;
+console.log(typeof result);
+console.log(result);
+
+let fruit = '사과';
+let apple = fruit;
+
+console.log(apple);
+
+let True = true;
+let False = false;
+
+let finalResult = True;
+let finalResult2 = False;
+
+// null 타입 - 부존재 (명시적인 없음)
+let money = null;
+console.log(money);
+// undefined - 변수가 아직 사용준비가 안됐다.
+let yourName;
+console.log(yourName);
+
+
+
+
+
